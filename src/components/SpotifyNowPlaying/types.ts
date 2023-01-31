ecae735079acf92dc6b5b3c540f0a498bf6fd89f
Anson <ansonlichtfuss@users.noreply.github.com>
2023-01-30 19:31:40 -0800

    add types not covered by spotify-api

---
+++ src/components/SpotifyNowPlaying/types.ts
@@ -0,0 +1,17 @@
+interface AlbumMetadata {
+  images: { url: string }[];
+  name: string,
+  artists: { name: string }[];
+}
+
+interface EpisodeMetadata {
+  images: { url: string }[];
+  name: string,
+  show: { name: string };
+}
+
+interface UiMetadata {
+  preview: string;
+  title: string;
+  subtitle: string;
+}
