907e02690fd94e9705a1bb653e8019a419bba460
Anson <ansonlichtfuss@users.noreply.github.com>
2023-03-21 16:49:42 -0700

    refactor: simplify root component hierarchy

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,32 @@
 import { A } from "solid-start";
-import Carousel from "~/components/Carousel";
 import { SpotifyAuthProvider } from "~/components/context/SpotifyAuthContext";
+import SpotifyNowPlaying from "~/components/SpotifyNowPlaying/SpotifyNowPlaying";
+
+const CARD_SIZE = 715;
 
 export default function Home() {
   return (
-    <main style={{ "font-family": "Inter" }}>
+    <main>
       <SpotifyAuthProvider>
-        <Carousel />
+        <div class="flex items-center justify-center w-screen h-screen bg-black text-white">
+          <div
+            class="relative flex"
+            style={{ flex: "none", width: `${CARD_SIZE}px`, height: `${CARD_SIZE}px` }}
+          >
+            <div
+              class="relative bg-black text-white rounded-3xl overflow-hidden"
+              style={{
+                flex: "none",
+                width: `${CARD_SIZE}px`,
+                height: `${CARD_SIZE}px`,
+                transform: `translateZ(0)`, // Fix for rounded corners in webkit
+                "scroll-snap-align": "center",
+              }}
+            >
+              <SpotifyNowPlaying />
+            </div>
+          </div>
+        </div>
       </SpotifyAuthProvider>
     </main>
   );
