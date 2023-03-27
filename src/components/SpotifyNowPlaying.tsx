7e59e58b81576809d97c41235ce618ee148c2d78
Anson <ansonlichtfuss@users.noreply.github.com>
2023-03-27 16:02:46 -0700

    chore: remove unused code

--- src/components/SpotifyNowPlaying.tsx
+++ src/components/SpotifyNowPlaying.tsx
@@ -151,58 +151,6 @@ const SpotifyNowPlaying: Component = () => {
         enableShuffle={!!stateContext.nowPlaying?.shuffle_state}
         enableSaved={false} // TODO
       />
-
-      {/*      <div
-        class="hidden fixed w-full left-0 bottom-0 flex items-center justify-between px-10"
-        style={{
-          "background-color": "rgba(0,0,0,0.2)",
-          height: "120px",
-          "align-items": "center",
-        }}
-      >
-        <div
-          class="absolute top-0 left-0 w-0 w-full origin-left"
-          style={{ height: "3px", "background-color": "rgba(0,0,0,0.2)" }}
-        ></div>
-        <div
-          class="absolute top-0 left-0 w-0 w-full scale-x-0 origin-left bg-white"
-          style={{ height: "3px", transform: `scaleX(${playingProgress()})` }}
-        ></div>
-        <PlayerControlIcon
-          src={SvgShuffle}
-          isDisabled={shouldDisableControls()}
-          showActiveIndicator={stateContext.nowPlaying?.shuffle_state}
-        />
-        <PlayerControlIcon
-          src={SvgSkipPrevious}
-          isDisabled={shouldDisableControls()}
-          enlargeIcon={true}
-        />
-        {stateContext.nowPlaying?.is_playing ? (
-          <PlayerControlIcon
-            src={SvgPause}
-            isDisabled={shouldDisableControls()}
-            enlargeIcon={true}
-            onClick={setPause}
-          />
-        ) : (
-          <PlayerControlIcon
-            src={SvgPlay}
-            isDisabled={shouldDisableControls()}
-            enlargeIcon={true}
-            onClick={setPlay}
-          />
-        )}
-        <PlayerControlIcon
-          src={SvgSkipNext}
-          isDisabled={shouldDisableControls()}
-          enlargeIcon={true}
-        />
-        <PlayerControlIcon
-          src={SvgEmptyHeart}
-          isDisabled={shouldDisableControls()}
-        />
-      </div>*/}
     </DynamicBackground>
   );
 };
