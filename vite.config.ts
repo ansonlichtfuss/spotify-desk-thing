9da8f070054bb6ea0d78606c64c8752efd6ee42f
Anson <ansonlichtfuss@users.noreply.github.com>
2026-08-23 18:06:29 -0700

    add nitro build target

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/solid-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
-
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
@@ -11,6 +11,7 @@ export default defineConfig({
 		devtools(),
 		tailwindcss(),
 		tanstackStart(),
+		nitro(),
 		solidPlugin({ ssr: true }),
 	],
 });
