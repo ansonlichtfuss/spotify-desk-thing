46ecebd0418e38a0331aa84e6f609e465f85d5fb
Anson <ansonlichtfuss@users.noreply.github.com>
2026-09-22 19:57:23 -0700

    fix local dev url

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
+	server: {
+		host: "127.0.0.1",
+		port: 3000,
+	},
 	resolve: { tsconfigPaths: true },
 	plugins: [
 		devtools(),
